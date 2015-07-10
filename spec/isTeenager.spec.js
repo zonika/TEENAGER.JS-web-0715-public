8dcd4602436bc955c89f33aad2ecb13649894d69
Katie Hoffman <ktahoffman@gmail.com>
2015-07-10 14:34:22 -0400

    remove extra describe wrapper

--- spec/isTeenager.spec.js
+++ spec/isTeenager.spec.js
@@ -1,17 +1,15 @@
 'use strict';
 
-describe('isTeenager', function() {
-  describe( "#is_between", function() {
-    it("returns false if the person is younger than 13", function() {
-      expect(isTeenager(12)).toBe(false);
-    });      
+describe( "#isTeenager", function() {
+  it("returns false if the person is younger than 13", function() {
+    expect(isTeenager(12)).toBe(false);
+  });      
 
-    it("returns false if the person is older than 19", function() {
-      expect(isTeenager(20)).toBe(false);
-    }); 
+  it("returns false if the person is older than 19", function() {
+    expect(isTeenager(20)).toBe(false);
+  }); 
 
-    it("returns true if the person is between the ages of 13 and 19", function() {
-      expect(isTeenager(15)).toBe(true);
-    });  
-  });
+  it("returns true if the person is between the ages of 13 and 19", function() {
+    expect(isTeenager(15)).toBe(true);
+  });  
 });
